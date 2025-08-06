4267f68b34d5c2bdf87a67868af12aee59632e53
Harry Newman <h.newman2005@outlook.com>
2025-08-06 22:19:17 +0100

    Notes + src/ folder

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+double z, n, c = 0;
+
+int main(void) {
+    printf("Mandelbrot Set Vis");
+
+    for (size_t i = 0; i < 15; ++i) {
+        z = (z * z) + 0;
+    }
+}
