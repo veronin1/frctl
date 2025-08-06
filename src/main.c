30a52aac00d2ee9579c71aeef3400c00a8d4e4ea
Harry Newman <h.newman2005@outlook.com>
2025-08-06 22:50:22 +0100

    Began visualising in raylib

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <complex.h>
+#include <raylib.h>
 #include <stdio.h>
 
+// raylib window related consts
 const int height = 800;
 const int width = 600;
+const char* title = "mandel";
+const int maxFPS = 60;
+
 double complex z, c = 0;
 double real, imaginary;
 const double max_real = 1;
@@ -11,7 +16,16 @@ const double max_imag = 1.5;
 const double min_imag = 1.5;
 
 int main(void) {
-    printf("Mandelbrot Set Vis");
+    InitWindow(height, width, title);
+    SetTargetFPS(maxFPS);
+
+    while (!WindowShouldClose()) {
+        int monitorWidth = GetMonitorWidth(GetCurrentMonitor());
+        int monitorHeight =
+            GetMonitorHeight(GetMonitorHeight(GetCurrentMonitor()));
+        int monitorFPS = GetMonitorRefreshRate(GetCurrentMonitor());
+    }
+    CloseWindow();
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
