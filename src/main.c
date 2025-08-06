c8f5a0d4bc53f1e2362ddc23ab53ecb58ac6bb96
Harry Newman <h.newman2005@outlook.com>
2025-08-06 22:30:55 +0100

    Moved definition of c to top of file

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 
 const int height = 800;
 const int width = 600;
-double complex z = 0;
+double complex z, c = 0;
 
 int main(void) {
     printf("Mandelbrot Set Vis");
 
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
-            double complex c = y + x * I;
+            c = y + x * I;
             z = (z * z) + c;
             printf("z = %f + %fi\n", creal(z), cimag(z));
         }
