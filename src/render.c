605909aa632aff5a0e63f1587c9a50bcf82c5efb
Harry Newman <h.newman2005@outlook.com>
2025-08-07 16:23:45 +0100

    Created abstracted render function

---
+++ src/render.c
@@ -0,0 +1,14 @@
+#include <raylib.h>
+
+typedef struct {
+    int width, height;
+    char* title;
+    int maxFPS;
+    double minReal, maxReal;
+    double minImag, maxImag;
+} RenderConfig;
+
+void RenderMandelbrot(const RenderConfig* cfg) {
+    InitWindow(cfg->width, cfg->height, cfg->title);
+    SetTargetFPS(cfg->maxFPS);
+}
