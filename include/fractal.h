a9f394a8868866fc31babdb3643a6238f5d00835
Harry Newman <h.newman2005@outlook.com>
2025-08-09 00:49:37 +0100

    Added fractalType to easily determine what type the fractal is, this commit is seemingly to be longer than it should

--- include/fractal.h
+++ include/fractal.h
@@ -3,8 +3,15 @@
 
 #include <stdlib.h>
 
+typedef enum {
+  FRACTAL_MANDELBROT,
+  FRACTAL_JULIA,
+  FRACTAL_BURNINGSHIP
+} FractalType;
+
 typedef struct {
-    double minReal, maxReal;
+  FractalType type;
+  double minReal, maxReal;
     double minImag, maxImag;
     size_t maxIter;
 } Fractal;
