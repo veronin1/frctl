f82bafbbc0d168d3e2db4d064dff1ce6a36d0bb5
Harry Newman <h.newman2005@outlook.com>
2025-08-08 21:24:27 +0100

    Use input values of width and height instead of making my own (?)

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -2,19 +2,16 @@
 #include <complex.h>
 #include <stdlib.h>
 
-const int height = 800;
-const int width = 800;
-
-int mandelbrot(Fractal* mandelbrot) {
-  for (int y = 0; y < height; ++y) {
-    for (int x = 0; x < width; ++x) {
+int mandelbrot(Fractal* mandelbrot, size_t width, size_t height) {
+  for (size_t y = 0; y < height; ++y) {
+    for (size_t x = 0; x < width; ++x) {
       // z_new = z_old * z_old + c
-      double cx =
-          ((double)x / width) * (mandelbrot->maxReal - mandelbrot->minReal) +
-          mandelbrot->minReal;
-      double cy =
-          ((double)y / height) * (mandelbrot->maxImag - mandelbrot->minImag) +
-          mandelbrot->minImag;
+      double cx = ((double)x / (double)width) *
+                      (mandelbrot->maxReal - mandelbrot->minReal) +
+                  mandelbrot->minReal;
+      double cy = ((double)y / (double)height) *
+                      (mandelbrot->maxImag - mandelbrot->minImag) +
+                  mandelbrot->minImag;
 
       double complex c = CMPLX(cx, cy);
       double complex z = 0;
