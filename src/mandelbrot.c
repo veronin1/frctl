c0f41a96f54ba2164ec8073a358cc3f002dc8eb0
Harry Newman <h.newman2005@outlook.com>
2025-08-08 21:10:12 +0100

    Use the CMPLX macro instead of making my own complex

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,25 +6,25 @@ const int height = 800;
 const int width = 800;
 
 int mandelbrot(Fractal* mandelbrot) {
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            // z_new = z_old * z_old + c
-            double cx = ((double)x / width) *
-                            (mandelbrot->maxReal - mandelbrot->minReal) +
-                        mandelbrot->minReal;
-            double cy = ((double)y / height) *
-                            (mandelbrot->maxImag - mandelbrot->minImag) +
-                        mandelbrot->minImag;
+  for (int y = 0; y < height; ++y) {
+    for (int x = 0; x < width; ++x) {
+      // z_new = z_old * z_old + c
+      double cx =
+          ((double)x / width) * (mandelbrot->maxReal - mandelbrot->minReal) +
+          mandelbrot->minReal;
+      double cy =
+          ((double)y / height) * (mandelbrot->maxImag - mandelbrot->minImag) +
+          mandelbrot->minImag;
 
-            double complex c = cx + cy * I;
-            double complex z = 0;
+      double complex c = CMPLX(cx, cy);
+      double complex z = 0;
 
-            size_t counter = 0;
-            while (cabs(z) <= 2 && counter < mandelbrot->maxIter) {
-                z = z * z + c;
-                ++counter;
-            }
-        }
+      size_t counter = 0;
+      while (cabs(z) <= 2 && counter < mandelbrot->maxIter) {
+        z = z * z + c;
+        ++counter;
+      }
     }
-    return MANDELBROT_SUCCESS;
+  }
+  return MANDELBROT_SUCCESS;
 }
