98561cb30b44b424a7a554ea785b8f2a320fd04a
Harry Newman <h.newman2005@outlook.com>
2025-08-09 18:04:18 +0100

    Added status codes for fractals

---
+++ include/status_codes.h
@@ -0,0 +1,17 @@
+#ifndef STATUS_CODES_H
+#define STATUS_CODES_H
+
+typedef enum {
+  FRACTAL_SUCCESS = 0,
+  FRACTAL_FAILURE = -1,
+  FRACTAL_ERR_NULL_POINTER = -2,
+  FRACTAL_ERR_INVALID_DIMENSIONS = -3,
+  FRACTAL_ERR_WRONG_TYPE = -4,
+
+  // fractacsl specfiic error codes start at 100
+  FRACTAL_ERR_BASE = -100,
+
+  // fractal (error = FRACTAL_ERR_BASE - 1...
+} FractalStatus;
+
+#endif // STATUS_CODES_H
