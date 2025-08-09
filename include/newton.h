96570d2556657a7518dbd4950bfa63abb181496a
Harry Newman <h.newman2005@outlook.com>
2025-08-10 00:54:40 +0100

    Newton fractal header

---
+++ include/newton.h
@@ -0,0 +1,12 @@
+#ifndef NEWTON_H
+#define NEWTON_H
+
+#include "fractal.h"
+#include <stdint.h>
+
+int newton(const Fractal *fractal, const size_t width, const size_t height,
+           uint16_t *iterBuffer);
+
+uint16_t newton_iter(const Fractal* fractal, size_t x, size_t y, const size_t width, const size_t height, uint16_t* iterBuffer);
+
+#endif // NEWTON.H
