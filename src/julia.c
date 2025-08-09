98884277e443170875d4e895e96cb2dc47539cc7
Harry Newman <h.newman2005@outlook.com>
2025-08-09 17:10:57 +0100

    Normalised the complex number (z)

--- src/julia.c
+++ src/julia.c
@@ -21,6 +21,19 @@ int julia(const Fractal* fractal, const size_t width, const size_t height) {
   if (fractal->type != FRACTAL_JULIA) {
     return JULIA_FAILURE;
   }
+
+  for (size_t y = 0; y < height; ++y) {
+    for (size_t x = 0; x < width; ++x) {
+      double zx =
+          ((double)x / (double)width) * (fractal->maxReal - fractal->minReal) +
+          fractal->minReal;
+      double zy =
+          ((double)y / (double)height) * (fractal->maxImag - fractal->minImag) +
+          fractal->minImag;
+      double complex c = 0;
+    }
+  }
+  return JULIA_SUCCESS;
 }
 
 uint16_t julia_iterint(const Fractal* fractal, size_t x, size_t y, size_t width,
