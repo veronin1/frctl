d931357434ad128b814e3e83107fecf015ea6bbd
Harry Newman <h.newman2005@outlook.com>
2025-08-09 01:17:30 +0100

    Added a helper iterator function for mandelbrot buffer calculations

--- include/mandelbrot.h
+++ include/mandelbrot.h
@@ -9,5 +9,7 @@
 #define MANDELBROT_FAILURE -1
 
 int mandelbrot(const Fractal* mandelbrot,const size_t width, const size_t height, uint16_t* iterBuffer);
+uint16_t mandelbrot_iter(const Fractal* fractal, size_t x, size_t y,
+                         size_t width, size_t height);
 
 #endif // MANDELBROT_H
--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -4,36 +4,41 @@
 #include "fractal.h"
 
 // Mandelbrot set: z_{n+1} = z_n^2 + c
-int mandelbrot(const Fractal* mandelbrot, const size_t width,
-               const size_t height, uint16_t* iterBuffer) {
-  if (!mandelbrot || !iterBuffer) {
+int mandelbrot(const Fractal* fractal, const size_t width, const size_t height,
+               uint16_t* iterBuffer) {
+  if (!fractal || !iterBuffer) {
     return MANDELBROT_FAILURE;
   }
 
-  if (mandelbrot->type != FRACTAL_MANDELBROT) {
+  if (fractal->type != FRACTAL_MANDELBROT) {
     return MANDELBROT_FAILURE;
   }
 
   for (size_t y = 0; y < height; ++y) {
     for (size_t x = 0; x < width; ++x) {
-      // map pixel coordinate (x,y) to complex plane coordinate (cx, cy)
-      double cx = ((double)x / (double)width) *
-                      (mandelbrot->maxReal - mandelbrot->minReal) +
-                  mandelbrot->minReal;
-      double cy = ((double)y / (double)height) *
-                      (mandelbrot->maxImag - mandelbrot->minImag) +
-                  mandelbrot->minImag;
-
-      double complex c = CMPLX(cx, cy);
-      double complex z = 0;
-
-      size_t counter = 0;
-      while (cabs(z) <= 2 && counter < mandelbrot->maxIter) {
-        z = z * z + c;
-        ++counter;
-      }
-      iterBuffer[y * width + x] = (uint16_t)counter;
+      iterBuffer[y * width + x] = mandelbrot_iter(fractal, x, y, width, height);
+      ;
     }
   }
   return MANDELBROT_SUCCESS;
+}
+
+uint16_t mandelbrot_iter(const Fractal* fractal, size_t x, size_t y,
+                         size_t width, size_t height) {
+  double cx =
+      ((double)x / (double)width) * (fractal->maxReal - fractal->minReal) +
+      fractal->minReal;
+  double cy =
+      ((double)y / (double)height) * (fractal->maxImag - fractal->minImag) +
+      fractal->minImag;
+
+  double complex c = CMPLX(cx, cy);
+  double complex z = 0;
+
+  size_t counter = 0;
+  while (cabs(z) <= 2 && counter < fractal->maxIter) {
+    z = z * z + c;
+    ++counter;
+  }
+  return (uint16_t)counter;
 }
