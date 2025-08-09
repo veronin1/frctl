243510aa5771e30d6f4632ca3c642dabdcdbc61b
Harry Newman <h.newman2005@outlook.com>
2025-08-09 17:29:17 +0100

    [format] updated header retrurn values

--- src/julia.c
+++ src/julia.c
@@ -6,14 +6,10 @@
 #include <complex.h>
 #include <stdint.h>
 
-#define JULIA_SUCCESS 0;
-#define JULIA_FAILURE -1;
-
 // example coords:
 // x,y
 // 0.7111476192
 // 0.2484631949
-
 int julia(const Fractal* fractal, const size_t width, const size_t height,
           uint16_t* iterBuffer) {
   if (!fractal || !height || !width) {
