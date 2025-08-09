b461ab08de4fab62ee2ce0e366461bf536fea24c
Harry Newman <h.newman2005@outlook.com>
2025-08-09 17:41:45 +0100

    [refactor] Adjusted real and imaginary factors for julia and saved mandelbrot examplar values as well to be adjusted later

--- src/main.c
+++ src/main.c
@@ -3,6 +3,21 @@
 #include "fractal.h"
 #include "render.h"
 
+/* MANDELBROT
+  fractal->minReal = -2.0;
+  fractal->maxReal = 1.0;
+  fractal->minImag = -1.5;
+  fractal->maxImag = 1.5;
+  fractal->maxIter = 1000;
+
+  JULIA
+  fractal->minReal = -1.5;
+  fractal->maxReal = 1.5;
+  fractal->minImag = -1.5;
+  fractal->maxImag = 1.5;
+  fractal->maxIter = 100;
+ */
+
 int main(void) {
   Fractal* fractal = malloc(sizeof(Fractal));
   if (!fractal) {
@@ -10,19 +25,19 @@ int main(void) {
   }
 
   fractal->type = FRACTAL_JULIA;
-  fractal->minReal = -2.0;
-  fractal->maxReal = 1.0;
+  fractal->minReal = -1.5;
+  fractal->maxReal = 1.5;
   fractal->minImag = -1.5;
   fractal->maxImag = 1.5;
-  fractal->maxIter = 1000;
+  fractal->maxIter = 100;
 
   RenderConfig* cfg = malloc(sizeof(RenderConfig));
   if (!cfg) {
     free(fractal);
     return 1;
   }
-  cfg->height = 800;
-  cfg->width = 800;
+  cfg->height = 1440;
+  cfg->width = 2560;
   cfg->title = "frctl";
   cfg->maxFPS = 60;
 
