67a81304944b535a15372db26bdbaa169aa9c09c
Harry Newman <h.newman2005@outlook.com>
2025-08-11 00:56:05 +0100

    Added test pattern

--- src/main.c
+++ src/main.c
@@ -31,20 +31,20 @@ int main(void) {
     return 1;
   }
 
-  fractal->type = FRACTAL_NEWTON;
-  fractal->minReal = -2.0;
-  fractal->maxReal = 2.0;
-  fractal->minImag = -2.0;
-  fractal->maxImag = 2.0;
-  fractal->maxIter = 300;
+  fractal->type = FRACTAL_JULIA;
+  fractal->minReal = -1.5;
+  fractal->maxReal = 1.5;
+  fractal->minImag = -1.5;
+  fractal->maxImag = 1.5;
+  fractal->maxIter = 10000;
 
   RenderConfig* cfg = malloc(sizeof(RenderConfig));
   if (!cfg) {
     free(fractal);
     return 1;
   }
-  cfg->height = 800;
-  cfg->width = 800;
+  cfg->height = 1440;
+  cfg->width = 2560;
   cfg->title = "frctl";
   cfg->maxFPS = 60;
 
