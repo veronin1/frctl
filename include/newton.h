72c2c65380a973cfced8c0eeaf65034ce6e507c4
Harry Newman <h.newman2005@outlook.com>
2025-08-10 17:01:48 +0100

    Fixed header

--- include/newton.h
+++ include/newton.h
@@ -7,6 +7,6 @@
 int newton(const Fractal *fractal, const size_t width, const size_t height,
            uint16_t *iterBuffer);
 
-uint16_t newton_iter(const Fractal* fractal, size_t x, size_t y, const size_t width, const size_t height, uint16_t* iterBuffer);
+uint16_t newton_iter(const Fractal* fractal, size_t x, size_t y, const size_t width, const size_t height);
 
 #endif // NEWTON.H
