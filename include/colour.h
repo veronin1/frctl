77417d0aaaf8004ad1508fdc2194e794d40f6f1f
Harry Newman <h.newman2005@outlook.com>
2025-08-10 22:41:50 +0100

    Mirrored changes from colour.c (header file function changes)

--- include/colour.h
+++ include/colour.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-float normaliseIterations(uint16_t* iterBuffer, size_t length);
-Color mapIterationToColor(uint16_t iteration, Color* colour);
+float* normaliseIterations(uint16_t* iterBuffer, size_t length);
+Color* mapIterationToColor(float normalisedValue, Color* colour);
 
 #endif
