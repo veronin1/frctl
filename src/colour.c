8513186b6e951851e95410b1ff0de52362e5609c
Harry Newman <h.newman2005@outlook.com>
2025-08-10 22:34:24 +0100

    Return normalisation as an array of values between 0 and 1
    Modified-by: Harry Newman <h.newman2005@outlook.com>

--- src/colour.c
+++ src/colour.c
@@ -3,17 +3,42 @@
 #include <raylib.h>
 #include <stdlib.h>
 
-float normaliseIterations(uint16_t* iterBuffer, size_t length) {
-  float min = 0, max = 0;
-
-  for (size_t i = 1; i < length; ++i) {
-    if (iterBuffer[i] < min)
-      min = iterBuffer[i];
-    if (iterBuffer[i] > max)
-      max = iterBuffer[i];
+float* normaliseIterations(uint16_t* iterBuffer, size_t length) {
+  if (length == 0) {
+    return NULL;
   }
+
+  uint16_t min = iterBuffer[0];
+  uint16_t max = iterBuffer[0];
+
+  // find min & max
+  for (size_t i = 1; i < length; ++i) {
+    if (iterBuffer[i] < min) {
+      min = iterBuffer[i];
+    }
+    if (iterBuffer[i] > max) {
+      max = iterBuffer[i];
+    }
+  }
+
+  // array of normalised floats
+  float* normalisedValues = malloc(length * sizeof(float));
+  if (!normalisedValues) {
+    return NULL;
+  }
+
+  // normalise as: norm = (original - min) / (max - min)
+  for (size_t i = 0; i < length; ++i) {
+    if (max == min) {
+      normalisedValues[i] = 0.0f;
+    } else {
+      normalisedValues[i] = (float)(iterBuffer[i] - min) / (max - min);
+    }
+  }
+  return normalisedValues;
 }
 
-Color mapIterationToColor(uint16_t iteration, Color* colour) {
-  float normalisedIteration = (iteration - 0) / (1 - 0) * 1;
+Color* mapIterationToColor(uint16_t iteration, Color* colour) {
+  // to codes
+  return colour;
 }
