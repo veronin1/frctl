8068f17aea632ca1c52090edbdd1cc6c7f90ad47
Harry Newman <h.newman2005@outlook.com>
2025-08-12 00:46:31 +0100

    Added float* normalisedValues as a param

--- include/colour.h
+++ include/colour.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-float* normaliseIterations(uint16_t* iterBuffer, size_t length);
+float* normaliseIterations(uint16_t* iterBuffer, size_t length, float* normalisedValues);
 int mapIterationToColor(float normalisedValue, Color* colour);
 void hsv_to_rgba(float h, float s, float v, uint8_t* r, uint8_t* g, uint8_t* b,
                  uint8_t* a, uint8_t alpha);
