3c49c0214bc7b09ab9d9cc8cd0625eafa141c807
Harry Newman <h.newman2005@outlook.com>
2025-08-12 20:39:15 +0100

    Use mandelbrot

--- src/main.c
+++ src/main.c
@@ -33,13 +33,12 @@ int main(void) {
   if (!fractal) {
     return 1;
   }
-
-  fractal->type = FRACTAL_NEWTON;
+  fractal->type = FRACTAL_MANDELBROT;
   fractal->minReal = -2.0;
-  fractal->maxReal = 2.0;
-  fractal->minImag = -2.0;
-  fractal->maxImag = 2.0;
-  fractal->maxIter = 20;
+  fractal->maxReal = 1.0;
+  fractal->minImag = -1.5;
+  fractal->maxImag = 1.5;
+  fractal->maxIter = 1000;
 
   RenderConfig* cfg = malloc(sizeof(RenderConfig));
   if (!cfg) {
